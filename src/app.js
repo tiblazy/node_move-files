ba565825af7f3bbd85d5f1d358d346f0584a8cb9
tiblazy <hauntedapply@gmail.com>
2025-03-06 05:09:13 -0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+function move(file, dest) {
+  try {
+    if (!file || !dest) {
+      throw new Error('Source and Destination must be provide.');
+    }
+
+    if (file === dest) {
+      return;
+    }
+
+    if (!fs.existsSync(file)) {
+      throw new Error('Source does not exists.');
+    }
+
+    if (fs.statSync(file).isDirectory()) {
+      throw new Error('Source is a directory, must be a file.');
+    }
+
+    let destPath = dest;
+
+    if (dest[dest.length - 1] === '/') {
+      destPath = path.join(dest, path.basename(file));
+    }
+
+    if (fs.existsSync(dest) && fs.statSync(dest).isDirectory()) {
+      destPath = path.join(dest, path.basename(file));
+    }
+
+    if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
+      throw new Error('Destination file already exists');
+    }
+
+    fs.renameSync(file, destPath);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const args = process.argv.slice(2);
+
+move(...args);
+
+module.exports = { move };
