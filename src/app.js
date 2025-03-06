ee8a507ad49cd6a4999d27b86e26d28987d48dd1
tiblazy <hauntedapply@gmail.com>
2025-03-06 05:22:08 -0300

    refact

--- src/app.js
+++ src/app.js
@@ -4,40 +4,60 @@ const path = require('path');
 
 function move(file, dest) {
   try {
-    if (!file || !dest) {
-      throw new Error('Source and Destination must be provide.');
-    }
+    checkArgs(file, dest);
+    checkFile(file);
 
-    if (file === dest) {
-      return;
-    }
+    const destPath = getDestinationPath(file, dest);
 
-    if (!fs.existsSync(file)) {
-      throw new Error('Source does not exists.');
-    }
-
-    if (fs.statSync(file).isDirectory()) {
-      throw new Error('Source is a directory, must be a file.');
-    }
-
-    let destPath = dest;
-
-    if (dest[dest.length - 1] === '/') {
-      destPath = path.join(dest, path.basename(file));
-    }
-
-    if (fs.existsSync(dest) && fs.statSync(dest).isDirectory()) {
-      destPath = path.join(dest, path.basename(file));
-    }
-
-    if (fs.existsSync(destPath) && fs.statSync(destPath).isFile()) {
-      throw new Error('Destination file already exists');
-    }
-
-    fs.renameSync(file, destPath);
+    checkDestination(destPath);
+    submit(file, destPath);
   } catch (error) {
     console.error(error.message);
   }
+}
+
+function checkArgs(file, dest) {
+  if (!file || !dest) {
+    throw new Error('Source and Destination must be provide.');
+  }
+
+  if (file === dest) {
+    process.exit(0);
+  }
+}
+
+function checkFile(file) {
+  if (!fs.existsSync(file)) {
+    throw new Error('Source does not exists.');
+  }
+
+  if (fs.statSync(file).isDirectory()) {
+    throw new Error('Source is a directory, must be a file.');
+  }
+}
+
+function getDestinationPath(file, dest) {
+  let destPath = dest;
+
+  if (dest[dest.length - 1] === '/') {
+    destPath = path.join(dest, path.basename(file));
+  }
+
+  if (fs.existsSync(dest) && fs.statSync(dest).isDirectory()) {
+    destPath = path.join(dest, path.basename(file));
+  }
+
+  return destPath;
+}
+
+function checkDestination(dest) {
+  if (fs.existsSync(dest) && fs.statSync(dest).isFile()) {
+    throw new Error('Destination file already exists');
+  }
+}
+
+function submit(file, destPath) {
+  fs.renameSync(file, destPath);
 }
 
 const args = process.argv.slice(2);
